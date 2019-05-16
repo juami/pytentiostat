384a37c5dd65ce67f94beb36b5510a60f0486574
YaoTomTong <47426666+YaoTomTong@users.noreply.github.com>
2019-05-16 13:58:49 -0500

    Updated Current_Code.py

--- docs/Current_Code.py
+++ docs/Current_Code.py
@@ -9,161 +9,110 @@ import serial
 import re
 import matplotlib.pyplot as plt
 
+class myArduinoBoard(object):
+    def __init__(self,portname = '/dev/tty.usbmodem14201'):
+        self.port = portname
 
-# ----Setting up Arduino board--------
+    def setup_arduino(self):
+        self.board = Arduino(self.port)
+        self.arduino = serial.Serial(self.port, 9600)
+        sleep(0.02)  # The synchronization between the Arduino board and pyFirmata requires some time. 1 = 1 second
+        self.analog_d9p = self.board.get_pin('d:9:p')  # Same as board.digital[9].mode = PWM
+        self.board.analog[0].mode = INPUT  # voltage
+        self.board.analog[2].mode = INPUT  # current
+        self.analog_d9p.frequency = 31372.25
+        self.it = util.Iterator(self.board)
+        self.it.start()
+        self.board.analog[0].enable_reporting()
+        self.board.analog[2].enable_reporting()
 
-port = '/dev/tty.usbmodem14201'# Find proper USB port on your laptop
-# if having error of: Can't open port, two reasons: 1. wrong port name. 2.Don't have permison
-# Go to Terminal type: sudo chmod 777 tty.usbmodem14201 (Use administrator status to change mode)
-# To find USB devices, go to terminal, type in cd dev, then ls
+    def readPin(self):
+        words = []
+        nums = []
+        while True:
+            line = self.arduino.readline()
+            if len(line) != 0:
+                words = re.findall(r'-?\d+.\d\d\d\d', line.decode('utf-8'))
+                nums = [float(w) for w in words]
+                print(nums)
+                if (len(nums) == 2):
+                    return nums
 
-board = Arduino(port)
-arduino = serial.Serial('/dev/tty.usbmodem14201', 9600)
-sleep(0.02)  # The synchronization between the Arduino board and pyFirmata requires some time. 1 = 1 second
+    def readData(self,x):
+        # Plotting array
+        ax = []
+        ay1 = []  # Define y value for Current
+        ay2 = []  # Define y value for Voltage
+        CurrentArray = []
+        VoltageArray = []
+        start = time.time()
+        lastHigh = time.time()
+        for i in range(0, int(x)):
+            last = time.time()
+            self.analog_d9p.write(i / 255)  # 100 % duty cycle (2.5V analog value)
+            val0 = 0 # Voltage
+            val2 = 0 # Current
+            nums = self.readPin()
+            val2 = nums[1]
+            val0 = nums[0]
+            CurrentArray.append(val2)
+            VoltageArray.append(val0)
+            time.sleep(0.001)
+            end = time.time()  # clock for the real time plotting
+            xl = end - start
+            # --------Plotting--------------
+            ax.append(round(xl,4))  # real time on x-axis
+            ay1.append(round(val2,4))  # assign Current with respect to xl
+            ay2.append(round(val0,4))  # assign Voltage with respect to xl
+        timeArray = numpy.asarray(ax)
+        CurrentArray = numpy.asarray(ay1)
+        VoltageArray = numpy.asarray(ay2)
+        numpy.savetxt('dataset.csv',numpy.column_stack((timeArray,CurrentArray,VoltageArray)))
+        return (ax,ay1,ay2)
 
-analog_d9p = board.get_pin('d:9:p') # Same as board.digital[9].mode = PWM
-board.analog[0].mode = INPUT  # voltage
-board.analog[2].mode = INPUT  # current
+    def plotData(self,x):
+        # Plotting array
+        ax = []
+        ay1 = []  # Define y value for Current
+        ay2 = []  # Define y value for Voltage
+        CurrentArray = []
+        VoltageArray = []
+        plt.ion()  # Open a plotting window
+        start = time.time()
+        for i in range(0, int(x)):
+            self.analog_d9p.write(i / 255)  # 100 % duty cycle (2.5V analog value)
+            nums = self.readPin()
+            val2 = nums[1] # Current
+            val0 = nums[0] # Voltage
+            CurrentArray.append(val2)
+            VoltageArray.append(val0)
+            time.sleep(0.001)
+            end = time.time()  # clock for the real time plotting
+            xl = end - start
+            # --------Plotting--------------
+            ax.append(xl)  # real time on x-axis
+            ay1.append(val2)  # assign Current with respect to xl
+            ay2.append(val0)  # assign Voltage with respect to xl
+            plt.clf()  # clear previous plots
 
-analog_d9p.frequency = 31372.25
+            plt.xlim(xl - 40, xl + 40)  # Set up x range
+            plt.ylim(-2500, 2500)  # Set up y range
 
-it = util.Iterator(board)
-it.start()
+            plt.plot(ax, ay1, color='blue')
+            plt.plot(ax, ay2, color='red')
 
-board.analog[0].enable_reporting()
-board.analog[2].enable_reporting()
+            plt.xlabel('Time')  # name x-axis
+            plt.ylabel('Current/Voltage')  # name y-axis
 
+            plt.pause(0.00001)  # pause for 0.01 second
+            plt.savefig('./testloop1.png')  # Save the poltting as a picture
+            plt.show()
+        plt.pause(10)
+        return (ax,ay1,ay2)
 
-
-# -------Port output testing-------------------------------------------
-# len=6
-# for i in range(0,len):
-#     board.analog[i].mode = INPUT  # voltage
-#     board.analog[i].enable_reporting()
-# # board.digital[9].write(0) # 100 % duty cycle (2.5V analog value)
-# # data = board.digital[9].read()
-# sleep(1)
-#
-# while(1):
-#     # board.digital[9].write(1)  # 100 % duty cycle (2.5V analog value)
-#     # print sard.readline()
-#     # print type(board.analog[0].read())
-#     dataList = [1,2,3,4,5,6]
-#     for i in range(0,len):
-#         dataList[i]=board.analog[i].read()
-#         # print board.analog[i].read()
-#     print dataList
-
-# -------------------------------------------------------------------
-
-
-
-
-
-
-# ----Defining reading function--------
-val0 = 0
-Voltage = 0
-Current = 0
-val0V = 0
-val2V = 0
-val2 = 0.0
-val9 = 0
-invi = 0
-CurrentArray = []
-VoltageArray = []
-
-# Plotting array
-ax = []
-ay1 = []  # Define y value for Current
-ay2 = []  # Define y value for Voltage
-plt.ion()  # Open a plotting window
-
-
-x=input("Please Enter a number:") # x is the duty cycle of PWM, usually type in 255
-
-mypwm = numpy.arange(0,1,0.004,dtype=float)
-
-def readPin():
-    words = []
-    nums = []
-    while True:
-        line = arduino.readline()
-        if len(line) != 0:
-            words = re.findall(r'-?\d+.\d\d\d\d', line.decode('utf-8'))
-            nums = [float(w) for w in words]
-            print (nums)
-            if(len(nums)==2):
-                return nums
-
-
-
-# --------Reading from Arduino board--------------
-
-start = time.time()
-lastHigh = time.time()
-for i in range(0, int(x)):
-    last = time.time()
-    analog_d9p.write(i / 255)  # 100 % duty cycle (2.5V analog value)
-    val0=0
-    val2=0
-
-# Average to smooth signal-------
-    # for j in range(1, 9):
-    #     nums = readPin()
-    #     val0 = val0 + nums[0]  # board.analog[0].read()
-    #     val2 = val2 + nums[1]  # board.analog[2].read()
-    #     # print(val2)
-    #     sleep(0.001)
-    # #print('val0: '+str(val0))
-    # #print('val2' +str(val2))
-    # val0 = val0 / 9
-    # val2 = val2 / 9
-#---------------------------------
-
-    nums = readPin()
-    val2 =nums[1]
-    val0 =nums[0]
-
-    CurrentArray.append(val2)
-    VoltageArray.append(val0)
-    time.sleep(0.001)
-
-#Calculations to get real currents and voltages-------------
-    # val0 = 511 - val0
-    # val2 = -1.0 * (511 - val2)
-    # val0V = val0 * 5.0
-    # val2V = val2 * 5.0
-    # Current = val2V * 1000.0 / (1024.0 * 0.216)  # Current from working electrode in uA set by resistor
-    # Voltage = val0V * 1000.0 / 1024  # Voltage from reference electrode in mV
-    #
-    # CurrentArray.append(Current)
-    # VoltageArray.append(Voltage)
-    # print('current'+str(Current))
-    # print('voltage' + str(Voltage))
-
-#------------------------------------------------------
-    end = time.time()#clock for the real time plotting
-    xl = end - start
-
-#--------Plotting--------------
-    ax.append(xl)  # real time on x-axis
-    ay1.append(val2)  # assign Current with respect to xl
-    ay2.append(val0)  # assign Current with respect to xl
-    plt.clf()                  # clear previous plots
-
-    plt.xlim(xl-40,xl+40)# Set up x range
-    plt.ylim(-2500,2500)# Set up y range
-
-    plt.plot(ax,ay1,color='blue')
-    plt.plot(ax,ay2,color='red')
-
-    plt.xlabel('Time') # name x-axis
-    plt.ylabel('Current/Voltage') # name y-axis
-
-    plt.pause(0.00001)  # pause for 0.01 second
-    plt.savefig('./testloop1.png')# Save the poltting as a picture
-    plt.show()
-
-
-plt.pause(10)
+if __name__ == '__main__':
+    test = myArduinoBoard()
+    test.setup_arduino()
+    x = input("Please Enter a number:")  # x is the duty cycle of PWM, usually type in 255
+    test.readData(x)
+    test.plotData(x)
