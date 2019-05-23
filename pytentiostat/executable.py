e3cb7de06340d1772679b8a47bff7ac9590647b3
YaoTomTong <47426666+YaoTomTong@users.noreply.github.com>
2019-05-23 12:51:38 -0500

    Create executable.py

---
+++ pytentiostat/executable.py
@@ -0,0 +1,19 @@
+import json
+from ArduinoBoard import ProgramSetup as PS
+#from DataHandeling import DataHandeling as DH
+
+
+
+if __name__ == '__main__':
+    test = PS()
+    test.setup_arduino()
+    with open('config.json') as json_data_file:
+        configdata = json.load(json_data_file)
+    ExperimentType = configdata['Experiment']['Type']
+    x = configdata[str(ExperimentType)]['Max_Vol']
+
+    PlotCommand = configdata['Plot']['Type']
+
+    WriteCommand = configdata['Write']['Type']
+
+    test.DataProcess(x,'plot','write')
