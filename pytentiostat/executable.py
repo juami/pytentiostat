d5012ff0633f7e211bf1e62057394ca356c5b20b
YaoTomTong <47426666+YaoTomTong@users.noreply.github.com>
2019-05-25 22:21:12 -0500

    Update executable.py

--- pytentiostat/executable.py
+++ pytentiostat/executable.py
@@ -1,20 +1,20 @@
 # This is what you want to execute
 import json
-from main import ProgramSetup as PS
+from ArduinoBoard import ProgramSetup as PS
 #from DataHandeling import DataHandeling as DH
 
 
 
 if __name__ == '__main__':
     test = PS()
-    test.setup_arduino()
+    #test.setup_arduino()
     with open('config.json') as json_data_file:
         configdata = json.load(json_data_file)
     ExperimentType = configdata['Experiment']['Type']
-    x = configdata[str(ExperimentType)]['Max_Volage']
+    x = configdata[str(ExperimentType)]['Max_Voltage']
 
     PlotCommand = configdata['Plot']['Type']
 
     WriteCommand = configdata['Write']['Type']
 
-    test.DataProcess(x,'plot','write')
+    test.DataProcess(x,PlotCommand,WriteCommand)
