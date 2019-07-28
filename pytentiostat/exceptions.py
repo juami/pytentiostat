9dab4ccb65d41d44200b5c672c32806f3a307b25
Mike Spencer <mspence2@ncsu.edu>
2019-07-28 08:38:13 -0400

    mike added some error messages to exceptions.py

--- pytentiostat/exceptions.py
+++ pytentiostat/exceptions.py
@@ -5,3 +5,16 @@ class error(Exception):
     """Base class for other exceptions"""
     pass
 
+class ValueContainsLetters(error)
+    """Raised when the input for numbers includes letters"""
+    pass
+
+class ValueContainsSpecialChar(error)
+    """Raised when the input contains special characters other than "." and "-"."""
+    pass
+
+class ValueOutsideLimits(error)
+    """Raised when input value is outside of the allowable input range"""
+    pass
+
+
