d235c094bdf24bb2db99e713412c486a015ff6a3
Mike Spencer <mspence2@ncsu.edu>
2019-07-28 08:29:26 -0400

    mike added an exceptions file that will screen user inputs

---
+++ pytentiostat/exceptions.py
@@ -0,0 +1,7 @@
+# This file is intended to screen all of the user inputs through a series of exceptions defined within a class.
+
+#create a user-defined error called Error derived from the Exception class
+class error(Exception):
+    """Base class for other exceptions"""
+    pass
+
