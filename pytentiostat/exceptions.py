58f3f925137892ce83c7ecbbef5236a302f141fb
Mike Spencer <mspence2@ncsu.edu>
2019-07-28 21:33:11 -0400

    mike added more exceptions to exceptions.py

--- pytentiostat/exceptions.py
+++ pytentiostat/exceptions.py
@@ -13,8 +13,31 @@ class ValueContainsSpecialChar(error)
     """Raised when the input contains special characters other than "." and "-"."""
     pass
 
-class ValueOutsideLimits(error)
-    """Raised when input value is outside of the allowable input range"""
+class DuplicateDecimals(error)
+    """Raised when more than one period is used in an input"""
     pass
 
+class DuplicateNegative(error)
+    """Raised when more than one negative sign is used in an input"""
+    pass
+# This one might be able to be combined with the last one depending on how the code is structured
 
+class ValueOutsidePotentialLimit(error)
+    """Raised when input potential is outside the potential bounds of the device"""
+    pass
+
+class LowerVoltageEqualsUpperVoltage(error)
+    """Raised when inputs in the lower turnover potential and upper turnover potential are equal"""
+    pass
+
+class InitialVoltageOutsideUserDefinedPotentialWindow(error)
+    """Raised when v_initial is outside the potential window defined by the v_ll and v_ul"""
+    pass
+
+class InitialVoltageOutsidePotentialLimit(error)
+    """Raised when the v_initial input is outside the potential bounds of the device"""
+    pass
+
+class TooManyDecimalPlaces(error)
+    """Raised when there are more than two decimal places in the input"""
+    pass
