78c95bb4fa346a92e76d00f63a7520df1d1f04a2
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-01 14:56:06 -0500

    Updating to work with new modular main

--- pytentiostat/Reporter.py
+++ pytentiostat/Reporter.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 29 10:30:15 2019
-
-@author: Austin
-"""
-
 import pandas as pd
 import shutil 
 
@@ -15,11 +8,9 @@ def exporter(Data):
     Export_File_Destination = "Place/holder/path"
     
     listdata = list(Data)
-    DF = pd.DataFrame(data = listdata, columns = ['Time(s)', 'Voltage(V)', 'Current(mA)'])
-    DF.to_csv(Filename,index=False,header=True)
+    dF = pd.DataFrame(data = listdata, columns = ['Time(s)', 'Voltage(V)', 'Current(mA)'])
+    dF.to_csv(Filename,index=False,header=True)
     
     #Uncomment after config updated
     #shutil.move(Filename, Export_File_Destination)
-    
-    
     
