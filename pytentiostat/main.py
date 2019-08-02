914dececee9d55735929c67377e912d844500c17
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-01 20:54:01 -0500

    Reformatted

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -1,17 +1,17 @@
 #Pytentiostat function files
-import Reporter as reporter
-import Tester as tester
-import Routines as routines
+import reporter as reporter
+import tester as tester
+import routines as routines
 
 #Initialize Experiment
-com, board, a0, a2, d9 = routines.StartupRoutine()
+com, board, a0, a2, d9 = routines.startup_routine()
 
 #Run the experiment and get the data
-Times, Voltages, Currents = tester.Experiment(board, a0, a2, d9)
+times, voltages, currents = tester.experiment(board, a0, a2, d9)
 
 #Generate a data report
-zippy = zip(Times, Voltages, Currents)
-reporter.exporter(zippy)
+collected_data = zip(times, voltages, currents)
+reporter.save_data_to_file(collected_data)
 
 #Wrap things up
-routines.ClosingRoutine(board, d9)
+routines.closing_routine(board, d9)
