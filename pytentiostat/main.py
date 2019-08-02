db35cddfd8846f7f30dfce985c4cac9b1fc682a8
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-02 08:54:38 -0500

    Changed function importing in main

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -1,17 +1,17 @@
 #Pytentiostat function files
-import reporter as reporter
-import tester as tester
-import routines as routines
+from reporter import save_data_to_file
+from tester import experiment
+from routines import startup_routine, closing_routine
 
 #Initialize Experiment
-com, board, a0, a2, d9 = routines.startup_routine()
+com, board, a0, a2, d9 = startup_routine()
 
 #Run the experiment and get the data
-times, voltages, currents = tester.experiment(board, a0, a2, d9)
+times, voltages, currents = experiment(board, a0, a2, d9)
 
 #Generate a data report
 collected_data = zip(times, voltages, currents)
-reporter.save_data_to_file(collected_data)
+save_data_to_file(collected_data)
 
 #Wrap things up
-routines.closing_routine(board, d9)
+closing_routine(board, d9)
