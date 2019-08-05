e6cfeddbb62cb2d2b58d147dbd0040e7aff4e68d
Simon Billinge <simon.billinge@gmail.com>
2019-08-05 05:57:47 -0400

    adapting startup routine for better testing and adding test

---
+++ tests/test_routines.py
@@ -0,0 +1,26 @@
+import mock
+import pytest
+
+from pytentiostat.routines import _load_arduino
+
+
+class Dummy_port():
+    def __init__(self, descr, com="com"):
+        self.description = descr
+        self.device = com
+
+
+def test_load_arduinos():
+    good_port = Dummy_port("Arduino Uno")
+    bad_port = Dummy_port("Not Arduino")
+    with mock.patch('pytentiostat.routines.serial.tools.list_ports.comports',
+                    return_value=[good_port]):
+        com = _load_arduino()
+        assert com == "com"
+    with pytest.raises(SystemExit):
+        with mock.patch('pytentiostat.routines.serial.tools.list_ports.comports',
+                        return_value=[bad_port]):
+            _load_arduino()
+    with pytest.raises(SystemExit):
+        _load_arduino()
+
