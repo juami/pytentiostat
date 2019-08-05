62cdf3f1b4b10c8290ef5f17d18af68d046c2afb
Simon Billinge <simon.billinge@gmail.com>
2019-08-05 06:24:41 -0400

    add matplotlib to requirements

--- tests/test_routines.py
+++ tests/test_routines.py
@@ -5,18 +5,19 @@ from pytentiostat.routines import _load_arduino
 
 
 class Dummy_port():
-    def __init__(self, descr, com="com"):
+    def __init__(self, descr):
         self.description = descr
-        self.device = com
+        self.device = "com"
 
 
 def test_load_arduinos():
     good_port = Dummy_port("Arduino Uno")
+    good_port.device = "good com"
     bad_port = Dummy_port("Not Arduino")
     with mock.patch('pytentiostat.routines.serial.tools.list_ports.comports',
                     return_value=[good_port]):
         com = _load_arduino()
-        assert com == "com"
+        assert com == "good com"
     with pytest.raises(SystemExit):
         with mock.patch('pytentiostat.routines.serial.tools.list_ports.comports',
                         return_value=[bad_port]):
