567f36cd80842f2e935584efab229cf113917b14
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-21 10:34:41 -0500

    STYL: made check and assertion on same line

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -8,5 +8,4 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_parse_config_files():
     confdir = os.path.join(THIS_DIR, 'static/')
     config_data = parse_config_file(confdir)
-    dict_check = isinstance(config_data, dict)
-    assert dict_check == True
+    assert isinstance(config_data, dict)
