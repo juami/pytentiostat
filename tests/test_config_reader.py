e4693fa1c21747335e2e95b0983e25e3b8c35a12
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-21 08:58:03 -0500

    STYL: Removed line under function in reader tester

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -6,7 +6,6 @@ from pytentiostat.config_reader import parse_config_file, get_adv_params
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test_parse_config_files():
-
     confdir = os.path.join(THIS_DIR, 'static/')
     config_data = parse_config_file(confdir)
     dict_check = isinstance(config_data, dict)
