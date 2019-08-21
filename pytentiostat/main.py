a00047559e97e988c56f803b3515eb24286e5fe6
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-21 08:15:51 -0500

    ENH: No more adv config in main
    Now only loading one file from parse config files function

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -4,13 +4,13 @@ from pytentiostat.reporter import save_data_to_file
 from pytentiostat.tester import experiment
 from pytentiostat.routines import startup_routine, closing_routine
 
-config_data, adv_config_data = parse_config_files()
+config_data = parse_config_file()
 
 # Initialize Experiment
 com, board, a0, a2, d9 = startup_routine()
 
 # Run the experiment and get the config_data
-times, voltages, currents = experiment(config_data, adv_config_data, board, a0, a2, d9)
+times, voltages, currents = experiment(config_data, board, a0, a2, d9)
 
 # Generate a config_data report
 collected_data = zip(times, voltages, currents)
