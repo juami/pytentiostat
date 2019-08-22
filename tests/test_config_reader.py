5b9d8a0dbc93563001f115154f0a780aa1574760
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-22 15:06:56 -0500

    ENH: now reader tester uses changing error output
    points to directory searched in with error output.

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -14,9 +14,9 @@ def test_parse_config_files():
         config_data = parse_config_file("Not_Real_Directory")
         assert pytest_exit_object.type == SystemExit 
         assert pytest_exit_object.value.code == 42  
-        assert pytest_exit_object.sys.stdout.getline().strip() == "Could not find config file. Exiting..."
+        assert pytest_exit_object.sys.stdout.getline().strip() == "Directory containing config file, {}, not found. Exiting...".format("Not_Real_Directory")
     with pytest.raises(SystemExit) as pytest_exit_object:
         config_data = parse_config_file(THIS_DIR)
         assert pytest_exit_object.type == SystemExit 
         assert pytest_exit_object.value.code == 42  
-        assert pytest_exit_object.sys.stdout.getline().strip() == "Could not find directory. Exiting..."
+        assert pytest_exit_object.sys.stdout.getline().strip() == "No file named config.yml found in config directory {}. Exiting...".format(THIS_DIR)
