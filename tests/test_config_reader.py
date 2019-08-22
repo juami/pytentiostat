6c773b2d46ed21a574b318e2d5cbb4ecd286a9b7
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-08-22 14:34:12 -0500

    ENH: Now test config reader checks dir and conf
    Now should test separately with distinguished exit statements

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -10,12 +10,13 @@ def test_parse_config_files():
     confdir = os.path.join(THIS_DIR, 'static/')
     config_data = parse_config_file(confdir)
     assert isinstance(config_data, dict)
-    with pytest.raises(SystemExit):
-        config_data = parse_config_file("Not_Real_Directory")
-    with pytest.raises(SystemExit):
-        config_data = parse_config_file(THIS_DIR)
     with pytest.raises(SystemExit) as pytest_exit_object:
         config_data = parse_config_file("Not_Real_Directory")
         assert pytest_exit_object.type == SystemExit 
         assert pytest_exit_object.value.code == 42  
-        assert pytest_exit_object.sys.stdout.getline().strip() == "Could not find directory or config. Exiting..."
+        assert pytest_exit_object.sys.stdout.getline().strip() == "Could not find config file. Exiting..."
+    with pytest.raises(SystemExit) as pytest_exit_object:
+        config_data = parse_config_file(THIS_DIR)
+        assert pytest_exit_object.type == SystemExit 
+        assert pytest_exit_object.value.code == 42  
+        assert pytest_exit_object.sys.stdout.getline().strip() == "Could not find directory. Exiting..."
