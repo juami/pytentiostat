dd4fa7f9d67d85b60c58f8057e0cd35812245373
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-09-17 15:47:53 -0500

    BUG: fixing test_config dictionary

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -23,7 +23,7 @@ def test_parse_config_file():
         assert pytest_exit_object.sys.stdout.getline().strip() == "No file named config.yml found in config directory {}. Exiting...".format(THIS_DIR)
 
 def test_get_output_params():
-    test_config = {"general_parameters": {"data_output_filename": 1.1, "data_output_path": 2.2}
+    test_config = {"general_parameters": {"data_output_filename": 1.1, "data_output_path": 2.2}}
     out_name, out_path = get_output_params(test_config)
     assert out_name == "1.1"
     assert out_path == "2.2"
