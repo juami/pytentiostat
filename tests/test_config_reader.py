cad3af61159c795d5f25bf22c2b68bb90039242e
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-09-17 15:35:58 -0500

    BUG: Changing mock config dictionary call
    Needed to call two entries for nested dictionary simultaneously at first level
    Changing mock_config to local dictionary inside test (Travis run made it appear as though global was called over mock patch version of dict

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -6,7 +6,6 @@ import mock
 from pytentiostat.config_reader import parse_config_file, get_adv_params, get_output_params
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-MOCK_CONFIG = {"general_parameters":{"data_output_filename":"Data"}, "general_parameters":{"data_output_path":"Desktop"}}
 
 def test_parse_config_file():
     confdir = os.path.join(THIS_DIR, 'static/')
@@ -24,7 +23,8 @@ def test_parse_config_file():
         assert pytest_exit_object.sys.stdout.getline().strip() == "No file named config.yml found in config directory {}. Exiting...".format(THIS_DIR)
 
 def test_get_output_params():
-    with mock.patch.dict(MOCK_CONFIG, {"general_parameters":{"data_output_filename":1.1}, "general_parameters":{"data_output_path":2.2}}):
+    mock_config = {"general_parameters": {"data_output_filename": "Data", "data_output_path": "Desktop"}}
+    with mock.patch.dict(mock_config, {"general_parameters": {"data_output_filename": 1.1, "data_output_path": 2.2}}):
         out_name, out_path = get_output_params(MOCK_CONFIG)
         assert out_name == "1.1"
         assert out_path == "2.2"
