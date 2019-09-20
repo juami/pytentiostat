8ad822e7d0f15acb091fba8664fbc0dd0553ac1b
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-09-20 11:11:12 -0500

    MAINT: Removing now redundant test
    removing previous test get out params

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -20,15 +20,6 @@ def test_parse_config_file():
         assert pytest_exit_object.type == SystemExit 
         assert pytest_exit_object.value.code == 42  
         assert pytest_exit_object.sys.stdout.getline().strip() == "No file named config.yml found in config directory {}. Exiting...".format(THIS_DIR)
-
-def test_get_output_params():
-    test_config = {"general_parameters": {"data_output_filename": 1.1, "data_output_path": 2.2}}
-    out_name, out_path = get_output_params(test_config)
-    check_out_name = out_name.split("_")
-    assert check_out_name[0] == "1.1"
-    check_out_path = out_path.split("_")
-    assert check_out_path[0] == "2.2"
-    assert out_name[-3:] == "csv"
     
 @pytest.mark.parametrize(
     "input,expected_name,expected_path",
