66604523c7b43120abe0bdb4ee77845283f81c02
jlhitt1993 <jlhitt1993@gmail.com>
2019-09-20 15:01:11 -0400

    removing unused argument from experiment call in main

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -20,7 +20,7 @@ board_instance = BoardCom()
 #com, board, a0, a2, d9 = startup_routine()
 
 # Run the experiment and get the config_data
-board_objects = (board_instance.board, board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
+board_objects = (board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
 times, voltages, currents = experiment(config_data, *board_objects)
 
 # Generate a config_data report
