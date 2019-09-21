f3d560746f92d51a63da8cd8a88d9e516d244cc2
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-09-21 14:23:19 -0500

    STYL: out_params doc strings
    Removing mt lines, capitalizing in descriptions, fixing typos.

--- pytentiostat/config_reader.py
+++ pytentiostat/config_reader.py
@@ -8,13 +8,12 @@ import os
 def parse_config_file(configlocation=None):
     """
     Reads config data from the config file
-
     the config file must be called config.yml.
 
     Parameters
     ----------
-    configlocation : str
-        the path on the filesystem to the config file.  Optional.  If not
+    configlocation : (str, optional)
+        The path on the filesystem to the config file. If not
         specified pytentiostat looks in the current directory for the
         config files.
 
@@ -22,8 +21,8 @@ def parse_config_file(configlocation=None):
     -------
     config_data : dict
         the configuration data
-
     """
+    
     if not configlocation:
         configlocation = "./"
     try:
@@ -48,21 +47,19 @@ def get_output_params(config_data, override_ts=None):
     Parameters
     ----------
     config_data : dict
-        the configuration data
-        
-    override_ts : Unique string to add after output filename in stringe. Optional.
-        If not specified will add a time stamp hours, minutes, and seconds the experiment
-        was conducted at as a unique identifier.
+        The configuration data
+    override_ts : (str, optional)
+        Unique string to append to output filename string.  If not specified will add a time stamp
+        hours, minutes, and seconds the experiment was conducted at as a unique identifier.
 
     Returns
     -------
     out_name_ts : str
-        the output filename with unique identifier.
-        
+        The output filename with unique identifier.    
     data_out_path : str
-        the path to which the output file will be written to.
-
+        The path to which the output file will be written to.
     """
+    
     data_out_name = config_data["general_parameters"]["data_output_filename"]
     data_out_path = config_data["general_parameters"]["data_output_path"]
     if data_out_path.lower() == "desktop":
@@ -83,20 +80,18 @@ def get_lsv_params(config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
     start_voltage : float
-        The voltage to start the sweep at in volts.
-        
+        The voltage to start the sweep at in volts.  
     end_voltage : float
         The voltage to end the sweep at in volts.
-        
     sweep_rate : float
         The rate at which the experiment is performed in millivolts per second.
-
     """
+    
     start_voltage = config_data["linear_sweep_voltammetry"]["start_voltage"]
     end_voltage = config_data["linear_sweep_voltammetry"]["end_voltage"]
     sweep_rate = config_data["linear_sweep_voltammetry"]["sweep_rate"]
@@ -122,6 +117,7 @@ def get_ca_params(config_data):
         How long the voltage will be held in seconds.
 
     """
+    
     voltage = config_data["chronoamperometry"]["voltage"]
     time = config_data["chronoamperometry"]["time"]
 
@@ -141,20 +137,16 @@ def get_cv_params(config_data):
     -------
     start_voltage : float
         The voltage to start the experiment at in volts.
-        
     first_turnover : float
-        The voltage at which the voltage sweep will first change directions in volts.
-        
+        The voltage at which the voltage sweep will first change directions in volts.     
     second_turnover: float
-        the second voltage at which the voltage sweep will change directions in volts.
-        
+        The second voltage at which the voltage sweep will change directions in volts.
     sweep_rate : float
-        The rate at which the experiment is performed in mV/s.
-        
+        The rate at which the experiment is performed in mV/s. 
     cycle_number: int
         How many times to perform this cycle.
-
     """
+    
     start_voltage = config_data["cyclic_voltammetry"]["start_voltage"]
     first_turnover = config_data["cyclic_voltammetry"]["first_turnover_voltage"]
     second_turnover = config_data["cyclic_voltammetry"]["second_turnover_voltage"]
@@ -171,14 +163,14 @@ def get_exp_type(config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
     exp_type : str
-        The type of experiment to be run
-
+        The type of experiment to be run.
     """
+    
     exp_type = config_data["general_parameters"]["experiment_type"]
 
     return exp_type
@@ -191,14 +183,14 @@ def get_exp_time(config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
     exp_time : float
         How long the experiment will be run in seconds.
-
     """
+    
     exp_time = config_data["chronoamperometry"]["time"]
 
     return exp_time
@@ -211,7 +203,7 @@ def get_rest(config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
@@ -219,6 +211,7 @@ def get_rest(config_data):
         How long the starting voltage will be held in seconds.
 
     """
+    
     rest_time = config_data["general_parameters"]["rest_time"]
 
     return rest_time
@@ -230,14 +223,14 @@ def get_steps(config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
     step_number : int
         How many points will be measured during the experiment
-
     """
+    
     step_number = config_data["general_parameters"]["step_number"]
     
     return step_number
@@ -250,7 +243,7 @@ def get_adv_params(adv_config_data):
     Parameters
     ----------
     config_data : dict
-        the configuration data
+        The configuration data
 
     Returns
     -------
@@ -264,8 +257,8 @@ def get_adv_params(adv_config_data):
         Minimum time to execute read/write cycle.
     average_number: int
         Number of times the measurement at each point will be performed.
-
     """
+    
     conversion_factor = adv_config_data["advanced_parameters"]["conversion_factor"]
     set_gain = adv_config_data["advanced_parameters"]["setpoint_gain"]
     set_offset = adv_config_data["advanced_parameters"]["setpoint_offset"]
@@ -281,6 +274,7 @@ def get_adv_params(adv_config_data):
         time_step,
         average_number,
     )
+
 def check_config_inputs(arg):
     """
     Checks that all the data that should be numerical from that config
@@ -295,8 +289,8 @@ def check_config_inputs(arg):
     _______
     is_number: Boolean
         Value is True if the arg is a number, False if not.
-
     """
+    
     try:
         return isinstance(float(arg), float)
     except:
