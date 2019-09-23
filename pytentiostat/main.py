08ce9d59c3c95a0e4eedc2f422a3c4e9d7c9ce55
jlhitt1993 <jlhitt1993@gmail.com>
2019-09-23 16:13:29 -0400

    Adding more handling for keyboard interupts

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -3,6 +3,7 @@ from pytentiostat.config_reader import parse_config_file
 from pytentiostat.reporter import save_data_to_file
 from pytentiostat.tester import experiment
 from pytentiostat.routines import startup_routine, closing_routine
+import sys
 
 config_data = parse_config_file()
 
@@ -15,17 +16,26 @@ class BoardCom:
         self.com, self.board, self.pin_a0, self.pin_a2, self.pin_d9 = startup_routine()
 
 
-# Initialize Experiment
-board_instance = BoardCom()
-# com, board, a0, a2, d9 = startup_routine()
+try:
+    # Initialize Experiment
+    board_instance = BoardCom()
+    # com, board, a0, a2, d9 = startup_routine()
+except KeyboardInterrupt:
+    sys.exit("Interrupted")
 
-# Run the experiment and get the config_data
-board_objects = (board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
-times, voltages, currents = experiment(config_data, *board_objects)
+try:
+    # Run the experiment and get the config_data
+    board_objects = (board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
+    times, voltages, currents = experiment(config_data, *board_objects)
+except KeyboardInterrupt:
+    sys.exit("Interrupted")
 
-# Generate a config_data report
-collected_data = zip(times, voltages, currents)
-save_data_to_file(config_data, collected_data)
+try:
+    # Generate a config_data report
+    collected_data = zip(times, voltages, currents)
+    save_data_to_file(config_data, collected_data)
+except KeyboardInterrupt:
+    sys.exit("Interupted")
 
 # Wrap things up
 closing_routine(board_instance.board, board_instance.pin_d9)
