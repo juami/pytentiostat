7c5090f3a2bc304ef031b6528a97334fea715857
jlhitt1993 <jlhitt1993@gmail.com>
2019-09-23 17:55:34 -0400

    Putting the experiment call in a loop and removing the newline chars

--- pytentiostat/main.py
+++ pytentiostat/main.py
@@ -5,7 +5,6 @@ from pytentiostat.tester import experiment
 from pytentiostat.routines import startup_routine, closing_routine
 import sys
 
-config_data = parse_config_file()
 
 
 class BoardCom:
@@ -16,26 +15,39 @@ class BoardCom:
         self.com, self.board, self.pin_a0, self.pin_a2, self.pin_d9 = startup_routine()
 
 
+config_data = parse_config_file()
+
 try:
     # Initialize Experiment
     board_instance = BoardCom()
     # com, board, a0, a2, d9 = startup_routine()
 except KeyboardInterrupt:
-    sys.exit("Interrupted")
+    sys.exit("Stopped. Make any changes and then restart the software.\nExiting...")
 
-try:
-    # Run the experiment and get the config_data
-    board_objects = (board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
-    times, voltages, currents = experiment(config_data, *board_objects)
-except KeyboardInterrupt:
-    sys.exit("Interrupted")
+board_objects = (board_instance.pin_a0, board_instance.pin_a2, board_instance.pin_d9)
+
+while True:
+    try:
+        # Run the experiment and get the config_data
+        times, voltages, currents = experiment(config_data, *board_objects)
+    except KeyboardInterrupt:
+        reconfig = input("Experiment stopped. To restart the same experiment, press Enter. \n"
+                         "If you want to do a different experiment, type \"New\"\n"
+                         "If you need to reconnect the poteniostat, type \"Reconnect\"")
+        if reconfig == "":
+            experiment(config_data, *board_objects)
+        if reconfig == "New":
+            break
+        if reconfig == "Reconnect":
+            break
+
 
 try:
     # Generate a config_data report
     collected_data = zip(times, voltages, currents)
     save_data_to_file(config_data, collected_data)
 except KeyboardInterrupt:
-    sys.exit("Interupted")
+    sys.exit("Interrupted")
 
 # Wrap things up
 closing_routine(board_instance.board, board_instance.pin_d9)
