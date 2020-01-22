ad8cc2780efcb04e7e1ea9915a97ac37fb0094fd
jlhitt1993 <jlhitt1993@gmail.com>
2020-01-22 10:51:14 -0500

    installing all dependencies

--- install_from_local.bat
+++ install_from_local.bat
@@ -15,3 +15,22 @@ pip install numpy-1.18.1-cp38-cp38-win_amd64.whl
 pip install pandas-0.25.3-cp38-cp38-win_amd64.whl
 pip install PyYAML-5.3-cp38-cp38-win_amd64.whl
 pip install matplotlib-3.1.2-cp38-cp38-win_amd64.whl
+pip install urllib3-1.25.8-py2.py3-none-any.whl
+pip install py-1.8.1-py2.py3-none-any.whl
+pip install idna-2.8-py2.py3-none-any.whl
+pip install chardet-3.0.4-py2.py3-none-any.whl
+pip install certifi-2019.11.28-py2.py3-none-any.whl
+pip install pluggy-0.13.1-py2.py3-none-any.whl
+pip install packaging-20.0-py2.py3-none-any.whl
+pip install more_itertools-8.1.0-py3-none-any.whl
+pip install wcwidth-0.1.8-py2.py3-none-any.whl
+pip install atomicwrites-1.3.0-py2.py3-none-any.whl
+pip install attrs-19.3.0-py2.py3-none-any.whl
+pip install coverage-5.0.3.tar.gz
+pip install colorama-0.4.3-py2.py3-none-any.whl
+pip install requests-2.22.0-py2.py3-none-any.whl
+pip install pytest-5.3.4-py3-none-any.whl
+pip install coverage-5.0.3-cp38-cp38m-win_amd64.whl
+pip install mock-3.0.5-py2.py3-none-any.whl
+pip install codecov-2.0.15-py2.py3-none-any.whl
+python pytentiostat/setup.py
