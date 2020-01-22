168a23c410646d33451e34501f0f954b612b0388
jlhitt1993 <jlhitt1993@gmail.com>
2020-01-22 13:58:28 -0500

    making installation more robust

--- local_install/install_from_local.bat
+++ local_install/install_from_local.bat
@@ -4,7 +4,6 @@ echo Installing python
 START /WAIT python-3.8.1-amd64
 python --version
 if errorlevel 1 goto NoPython
-pip install cycler-0.10.0-py2.py3-none-any.whl
 pip install pytz-2019.3-py2.py3-none-any.whl
 pip install kiwisolver-1.1.0-cp38-none-win_amd64.whl
 pip install six-1.14.0-py2.py3-none-any.whl
@@ -15,7 +14,6 @@ pip install pyFirmata-1.1.0-py2.py3-none-any.whl
 pip install numpy-1.18.1-cp38-cp38-win_amd64.whl
 pip install pandas-0.25.3-cp38-cp38-win_amd64.whl
 pip install PyYAML-5.3-cp38-cp38-win_amd64.whl
-pip install matplotlib-3.1.2-cp38-cp38-win_amd64.whl
 pip install urllib3-1.25.8-py2.py3-none-any.whl
 pip install py-1.8.1-py2.py3-none-any.whl
 pip install idna-2.8-py2.py3-none-any.whl
@@ -27,16 +25,18 @@ pip install more_itertools-8.1.0-py3-none-any.whl
 pip install wcwidth-0.1.8-py2.py3-none-any.whl
 pip install atomicwrites-1.3.0-py2.py3-none-any.whl
 pip install attrs-19.3.0-py2.py3-none-any.whl
+pip install cycler-0.10.0-py2.py3-none-any.whl
+pip install matplotlib-3.1.2-cp38-cp38-win_amd64.whl
 pip install coverage-5.0.3.tar.gz
 pip install colorama-0.4.3-py2.py3-none-any.whl
 pip install requests-2.22.0-py2.py3-none-any.whl
 pip install pytest-5.3.4-py3-none-any.whl
-pip install coverage-5.0.3-cp38-cp38m-win_amd64.whl
 pip install mock-3.0.5-py2.py3-none-any.whl
 pip install codecov-2.0.15-py2.py3-none-any.whl
-python pytentiostat/setup.py
+python pytentiostat/setup.py install
+@pause
 goto:eof
 
 :NoPython
 echo Python installation failed. Please retry.
-
+goto:eof
