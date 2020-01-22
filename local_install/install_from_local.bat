b993c5bad254657608c237bbb9c0504d1eb62e76
jlhitt1993 <jlhitt1993@gmail.com>
2020-01-22 16:32:48 -0500

    helping setup find files

--- local_install/install_from_local.bat
+++ local_install/install_from_local.bat
@@ -4,36 +4,37 @@ echo Installing python
 START /WAIT python-3.8.1-amd64
 python --version
 if errorlevel 1 goto NoPython
-pip install pytz-2019.3-py2.py3-none-any.whl
-pip install kiwisolver-1.1.0-cp38-none-win_amd64.whl
-pip install six-1.14.0-py2.py3-none-any.whl
-pip install pyparsing-2.4.6-py2.py3-none-any.whl
-pip install python_dateutil-2.8.1-py2.py3-none-any.whl
-pip install pyserial-3.4-py2.py3-none-any.whl
-pip install pyFirmata-1.1.0-py2.py3-none-any.whl
-pip install numpy-1.18.1-cp38-cp38-win_amd64.whl
-pip install pandas-0.25.3-cp38-cp38-win_amd64.whl
-pip install PyYAML-5.3-cp38-cp38-win_amd64.whl
-pip install urllib3-1.25.8-py2.py3-none-any.whl
-pip install py-1.8.1-py2.py3-none-any.whl
-pip install idna-2.8-py2.py3-none-any.whl
-pip install chardet-3.0.4-py2.py3-none-any.whl
-pip install certifi-2019.11.28-py2.py3-none-any.whl
-pip install pluggy-0.13.1-py2.py3-none-any.whl
-pip install packaging-20.0-py2.py3-none-any.whl
-pip install more_itertools-8.1.0-py3-none-any.whl
-pip install wcwidth-0.1.8-py2.py3-none-any.whl
-pip install atomicwrites-1.3.0-py2.py3-none-any.whl
-pip install attrs-19.3.0-py2.py3-none-any.whl
-pip install cycler-0.10.0-py2.py3-none-any.whl
-pip install matplotlib-3.1.2-cp38-cp38-win_amd64.whl
-pip install coverage-5.0.3.tar.gz
-pip install colorama-0.4.3-py2.py3-none-any.whl
-pip install requests-2.22.0-py2.py3-none-any.whl
-pip install pytest-5.3.4-py3-none-any.whl
-pip install mock-3.0.5-py2.py3-none-any.whl
-pip install codecov-2.0.15-py2.py3-none-any.whl
-python pytentiostat/setup.py install
+pip install whls/pytz-2019.3-py2.py3-none-any.whl
+pip install whls/kiwisolver-1.1.0-cp38-none-win_amd64.whl
+pip install whls/six-1.14.0-py2.py3-none-any.whl
+pip install whls/pyparsing-2.4.6-py2.py3-none-any.whl
+pip install whls/python_dateutil-2.8.1-py2.py3-none-any.whl
+pip install whls/pyserial-3.4-py2.py3-none-any.whl
+pip install whls/pyFirmata-1.1.0-py2.py3-none-any.whl
+pip install whls/numpy-1.18.1-cp38-cp38-win_amd64.whl
+pip install whls/pandas-0.25.3-cp38-cp38-win_amd64.whl
+pip install whls/PyYAML-5.3-cp38-cp38-win_amd64.whl
+pip install whls/urllib3-1.25.8-py2.py3-none-any.whl
+pip install whls/py-1.8.1-py2.py3-none-any.whl
+pip install whls/idna-2.8-py2.py3-none-any.whl
+pip install whls/chardet-3.0.4-py2.py3-none-any.whl
+pip install whls/certifi-2019.11.28-py2.py3-none-any.whl
+pip install whls/pluggy-0.13.1-py2.py3-none-any.whl
+pip install whls/packaging-20.0-py2.py3-none-any.whl
+pip install whls/more_itertools-8.1.0-py3-none-any.whl
+pip install whls/wcwidth-0.1.8-py2.py3-none-any.whl
+pip install whls/atomicwrites-1.3.0-py2.py3-none-any.whl
+pip install whls/attrs-19.3.0-py2.py3-none-any.whl
+pip install whls/cycler-0.10.0-py2.py3-none-any.whl
+pip install whls/matplotlib-3.1.2-cp38-cp38-win_amd64.whl
+pip install whls/coverage-5.0.3.tar.gz
+pip install whls/colorama-0.4.3-py2.py3-none-any.whl
+pip install whls/requests-2.22.0-py2.py3-none-any.whl
+pip install whls/pytest-5.3.4-py3-none-any.whl
+pip install whls/mock-3.0.5-py2.py3-none-any.whl
+pip install whls/codecov-2.0.15-py2.py3-none-any.whl
+cd ..
+python setup.py install
 @pause
 goto:eof
 
