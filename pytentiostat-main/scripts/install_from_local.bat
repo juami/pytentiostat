ddd7d1334fb8bdb6c86b9af9edfae15bfc1414a7
Abangma Jessika <127256410+InongoMag@users.noreply.github.com>
2025-01-22 04:50:08 +0100

    commit

--- pytentiostat-main/scripts/install_from_local.bat
+++ pytentiostat-main/scripts/install_from_local.bat
@@ -1,39 +1,47 @@
-:: This batch script will install python 3.8, pytentiostat and it's necessary packages
+:: This batch script will install Python 3.12, pytentiostat, and its necessary packages
 @echo off
 cd ../local_install
-echo Installing python
-START /WAIT python-3.8.1-amd64
+echo Installing Python 3.12
+START /WAIT python-3.12.8-amd64.exe
 python --version
 if errorlevel 1 goto NoPython
-pip install whls/pytz-2019.3-py2.py3-none-any.whl
-pip install whls/kiwisolver-1.1.0-cp38-none-win_amd64.whl
-pip install whls/six-1.14.0-py2.py3-none-any.whl
-pip install whls/pyparsing-2.4.6-py2.py3-none-any.whl
-pip install whls/python_dateutil-2.8.1-py2.py3-none-any.whl
-pip install whls/pyserial-3.4-py2.py3-none-any.whl
-pip install whls/pyFirmata-1.1.0-py2.py3-none-any.whl
-pip install whls/numpy-1.18.1-cp38-cp38-win_amd64.whl
-pip install whls/pandas-0.25.3-cp38-cp38-win_amd64.whl
-pip install whls/PyYAML-5.3-cp38-cp38-win_amd64.whl
-pip install whls/urllib3-1.25.8-py2.py3-none-any.whl
-pip install whls/py-1.8.1-py2.py3-none-any.whl
-pip install whls/idna-2.8-py2.py3-none-any.whl
-pip install whls/chardet-3.0.4-py2.py3-none-any.whl
-pip install whls/certifi-2019.11.28-py2.py3-none-any.whl
-pip install whls/pluggy-0.13.1-py2.py3-none-any.whl
-pip install whls/packaging-20.0-py2.py3-none-any.whl
-pip install whls/more_itertools-8.1.0-py3-none-any.whl
-pip install whls/wcwidth-0.1.8-py2.py3-none-any.whl
-pip install whls/atomicwrites-1.3.0-py2.py3-none-any.whl
-pip install whls/attrs-19.3.0-py2.py3-none-any.whl
-pip install whls/cycler-0.10.0-py2.py3-none-any.whl
-pip install whls/matplotlib-3.1.2-cp38-cp38-win_amd64.whl
-pip install whls/coverage-5.0.3.tar.gz
-pip install whls/colorama-0.4.3-py2.py3-none-any.whl
-pip install whls/requests-2.22.0-py2.py3-none-any.whl
-pip install whls/pytest-5.3.4-py3-none-any.whl
-pip install whls/mock-3.0.5-py2.py3-none-any.whl
-pip install whls/codecov-2.0.15-py2.py3-none-any.whl
+
+:: Upgrade pip to the latest version
+python -m pip install --upgrade pip
+
+:: Install necessary packages
+pip install pytentiostat
+pip install pytz
+pip install kiwisolver
+pip install six
+pip install pyparsing
+pip install python_dateutil
+pip install pyserial
+pip install pyFirmata
+pip install numpy
+pip install pandas
+pip install PyYAML
+pip install urllib3
+pip install py
+pip install idna
+pip install chardet
+pip install certifi
+pip install pluggy
+pip install packaging
+pip install more_itertools
+pip install wcwidth
+pip install atomicwrites
+pip install attrs
+pip install cycler
+pip install matplotlib
+pip install coverage
+pip install colorama
+pip install requests
+pip install pytest
+pip install mock
+pip install codecov
+pip install PySide6
+
 cd ..
 python setup.py install
 @pause
